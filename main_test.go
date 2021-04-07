333979efbfab62794d2bb941e79a5438a26cc732
Christoph Gattermayr <christoph.gattermayr@outlook.com>
2021-04-07 10:47:33 +0200

    Update test credentials for postgres

--- main_test.go
+++ main_test.go
@@ -15,10 +15,17 @@ import (
 var a App
 
 func TestMain(m *testing.M) {
+	// local tests
+	// a.Initialize(
+	// 	os.Getenv("APP_DB_USERNAME"),
+	// 	os.Getenv("APP_DB_PASSWORD"),
+	// 	os.Getenv("APP_DB_NAME"))
+
+	// travis ci tests
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"",
+		"postgres")
 
 	ensureTableExists()
 	code := m.Run()
